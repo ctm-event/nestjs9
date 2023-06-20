f4b724f0807b246f034cfa534370c6e86a8d5e3e
unknown <Steven.Nguyen@contemi.com>
2023-06-20 17:52:10 +0700

    61

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @Post('login')
   @UseGuards(AuthGuard('local'))
   async login(@Request() request) {
-    return request.user;
+    return {
+      userId: request.user.id,
+      token: this.authService.getTokenForUser(request.user),
+    };
   }
 }

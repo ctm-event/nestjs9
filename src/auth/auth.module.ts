4d375a7a81ef2e3a8833b6851afa276757df2ef4
Steven Nguyenm <nguyenbaotrung.hcm@gmail.com>
2023-06-22 23:22:14 +0700

    65

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { User } from './user.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [
@@ -20,6 +21,6 @@ import { JwtStrategy } from './jwt.strategy';
     }),
   ],
   providers: [LocalStrategy, JwtStrategy, AuthService],
-  controllers: [AuthController],
+  controllers: [AuthController, UserController],
 })
 export class AuthModule {}
--- src/auth/user.controller.ts
+++ src/auth/user.controller.ts
@@ -2,9 +2,15 @@ import { Controller, Post, Body, BadRequestException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
 import { CreateUserDto } from './input/create.user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 @Controller('users')
 export class UserController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
 
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
@@ -13,5 +19,27 @@ export class UserController {
     if (createUserDto.password !== createUserDto.retypedPassword) {
       throw new BadRequestException(['Passwords are not identical']);
     }
+
+    const existingUser = await this.userRepository.findOne({
+      where: [
+        { username: createUserDto.username },
+        { email: createUserDto.email },
+      ],
+    });
+
+    if (!!existingUser) {
+      throw new BadRequestException(['username or email is already taken']);
+    }
+
+    user.username = createUserDto.username;
+    user.password = await this.authService.hashPassword(createUserDto.password);
+    user.email = createUserDto.email;
+    user.firstName = createUserDto.firstName;
+    user.lastName = createUserDto.lastName;
+
+    return {
+      ...(await this.userRepository.save(user)),
+      token: this.authService.getTokenForUser(user),
+    };
   }
 }
